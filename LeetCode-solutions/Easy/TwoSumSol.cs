2a65634da6667f8dc05a0d064b00b695939c14b7
Masumul Hassan <contact@masum.me>
2022-06-13 03:39:18 +0600

    Add TwoSum easy solution

---
+++ LeetCode-solutions/Easy/TwoSumSol.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_solutions
+{
+    public class TwoSumSol
+    {
+        public static void Main(string[] args)
+        {
+            int[] IntArray = new int[] {2,7,11,15};
+            int target = 9;
+            
+            var result = TwoSum(IntArray, target);
+            Console.Out.Write(result);
+        }
+        
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> previous = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (previous.ContainsKey(target - nums[i]))
+                {
+                    return new[] {i, previous[target - nums[i]]};
+                }
+
+                if (!previous.ContainsKey(nums[i]))
+                {
+                    previous.Add(nums[i], i);
+                }
+            }
+
+            return new []{0};
+        }
+    }
+}
